42a9c9b084cea3b7a1b8d12f98c719ad4713f1ef
shorewind <99942150+shorewind@users.noreply.github.com>
2022-06-04 19:15:14 -0500

    Add assets and style

---
+++ script.js
@@ -0,0 +1,20 @@
+function show(){
+    let image = document.getElementById("image");
+    image.src = "assets/image.png"
+
+    document.getElementById("button").style.display = "none";
+    document.getElementById("hello").style.display = "none";
+    document.getElementById("intro").style.display = "none";
+
+    let music = new Audio("assets/music.mp3");
+    music.addEventListener("ended", function() {
+        this.currentTime = 0;
+        this.play();
+    })
+    music.play();
+} 
+
+function date(){
+    const options = {month: "long", day: "numeric", year: "numeric"}
+    document.write(new Date().toLocaleDateString(undefined, options));
+}
